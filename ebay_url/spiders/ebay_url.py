785b25283c489fa49020185f4112f412ac71fb0c
Hiro Saito <aiguozhe01@gmail.com>
2020-06-17 10:17:18 +0900

    Setting up Spiders 01

--- ebay_url/spiders/ebay_url.py
+++ ebay_url/spiders/ebay_url.py
@@ -1,3 +1,21 @@
 import scrapy
 
-class
+from scrapy.spiders import CrawlSpider
+
+class EbayUrlSpider(CrawlSpider):
+
+    #
+    name = 'ebay_url'
+
+    #
+    allowed_domains = ['ebay.com']
+
+    #
+    start_urls = ['https://www.ebay.com']
+
+    #
+    rules = [Rule(LinkExtractor(allow=''), callback='parse_httpresponse', follow=True)]
+
+    def parse_httpresponse():
+        
+        return
